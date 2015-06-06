574d517ae175a001087174424ac053ea3f370606
RalphLeague <ralphlolbol@gmail.com>
2015-06-06 01:22:49 -0400

    update2

---
+++ js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var ul = document.querySelector('ul');
+	for (var i = ul.children.length; i >= 0; i--) {
+		ul.appendChild(ul.children[Math.random() * i | 0]);
+	} 
+	
+	$('ul').fadeTo( "slow" , 1);
+});
+
