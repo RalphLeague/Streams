cef855ed4d8ad83dfa8e431aaad0410475381b24
RalphLeague <ralphlolbol@gmail.com>
2015-06-06 11:13:17 -0400

    u15

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
+$(window).load(function() {
+	$(".se-pre-con").fadeOut("slow");;
+});
+
 $(document).ready(function(){
-	$(".se-pre-con").fadeOut("slow");
-	
 	var ul = document.querySelector('ul');
 	for (var i = ul.children.length; i >= 0; i--) {
 		ul.appendChild(ul.children[Math.random() * i | 0]);
 	} 
-
-
 });
 
