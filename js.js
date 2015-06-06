67c0904c5d80771c866947d343747ad0c44e5442
RalphLeague <ralphlolbol@gmail.com>
2015-06-06 11:18:51 -0400

    u16

--- js.js
+++ js.js
@@ -1,9 +1,7 @@
 $(window).load(function() {
-	// Animate loader off screen
-	$("#loader").animate({
-		top: -200
-	}, 1500);
+	$(".se-pre-con").fadeOut("slow");;
 });
+
 $(document).ready(function(){
 	var ul = document.querySelector('ul');
 	for (var i = ul.children.length; i >= 0; i--) {
