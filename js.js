7be693d6c36d922f92fe8bbdb8ff1e4f5832513b
RalphLeague <ralphlolbol@gmail.com>
2015-06-08 13:10:46 -0400

    u20

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$(".se-pre-con").delay(300).fadeOut("slow");
+	$(".se-pre-con").delay(400).fadeOut("slow");
 	var ul = document.querySelector('ul');
 	for (var i = ul.children.length; i >= 0; i--) {
 		ul.appendChild(ul.children[Math.random() * i | 0]);
