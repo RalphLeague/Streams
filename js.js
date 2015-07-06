49d572356a43340912ba8b1d7f06829cf6557138
RalphLeague <ralphlolbol@gmail.com>
2015-07-06 17:03:10 -0400

    up1

--- js.js
+++ js.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
+	var found = false;
+	$.ajaxSetup({ cache: false });
+	
 	$('.panel').each(function(index, current){
 		$(current).hide();
 	});
+	
 	$(".se-pre-con").delay(600).fadeOut(1500);
 	var ul = document.querySelector('ul');
 	for (var i = ul.children.length; i >= 0; i--) {
@@ -9,9 +13,11 @@ $(document).ready(function(){
 	} 
 	
 	$('.panel').each(function(index, item){
-		ajaxStuffShow($(item).data('id'), item);
+		ajax($(item).data('id'), item, true);
 	});
-
+	if (found = false) {
+		$("#found").html("<h1>No Streams Currently Live</h1>");
+	}
 	var clicked = 0;
 	$("#onoffswitch").click(function(){
 		if (clicked == 0){
@@ -21,55 +27,26 @@ $(document).ready(function(){
 			clicked = 1;
 		} else {
 			$('.panel').each(function(index, item){
-				ajaxStuffHide($(item).data('id'), item);
+				ajax($(item).data('id'), item, false);
 			});
 			clicked = 0;
 		}
 	});
 });
 
-function ajaxStuffShow(id, current){
-	var result;
-	var xmlhttp;
-	if (window.XMLHttpRequest){
-		// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	} else {
-		// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	
-	xmlhttp.onreadystatechange=function(){
-		if (xmlhttp.readyState==4 && xmlhttp.status==200){
-			if (xmlhttp.responseText == '{"onair":true}'){
-				$(current).show();
-			}
+function ajax(id, current, show){
+	$.ajax({
+	  type:     "GET",
+	  url:      "https://api.dailymotion.com/video/"+id+"?fields=onair",
+	  dataType: "jsonp",
+	  success: function(data){
+		if (data.onair == true && show == true){
+			$(current).show();
+			var found = true;
+		} else if (data.onair == false && show == false){
+			$(current).hide();
+			var found = true;
 		}
-	}
-
-	xmlhttp.open("GET","https://api.dailymotion.com/video/"+id+"?fields=onair "+Math.floor((Math.random()*100)+1),true);
-	xmlhttp.send();	
-}
-
-function ajaxStuffHide(id, current){
-	var result;
-	var xmlhttp;
-	if (window.XMLHttpRequest){
-		// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	} else {
-		// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	
-	xmlhttp.onreadystatechange=function(){
-		if (xmlhttp.readyState==4 && xmlhttp.status==200){
-			if (xmlhttp.responseText == '{"onair":false}'){
-				$(current).hide();
-			}
-		}
-	}
-
-	xmlhttp.open("GET","https://api.dailymotion.com/video/"+id+"?fields=onair "+Math.floor((Math.random()*100)+1),true);
-	xmlhttp.send();	
+	  }
+	});
 }
